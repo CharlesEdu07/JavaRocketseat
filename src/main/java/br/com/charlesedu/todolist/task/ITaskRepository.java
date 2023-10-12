d8360f32fad09267131a4da0c4255998c09cf16d
unknown <charleseduardofaria@gmail.com>
2023-10-12 13:27:53 -0300

    Listando tarefas do usuário

--- src/main/java/br/com/charlesedu/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/charlesedu/todolist/task/ITaskRepository.java
@@ -4,8 +4,9 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-    
+    List<TaskModel> findByIdUser(UUID idUser);
 }
